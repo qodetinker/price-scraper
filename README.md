738673f0f69d0559fc0d549a6183092874e0dd65
Qode Tinker <philipquach@outlook.com>
2021-03-30 20:32:02 +0000

    updated with dictionaries

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# price-scrapper
-Scraping Nike.com for interested items
+# Project Name: price-scrapper
+Scraping Nike.com for interested items. Written in replit and uploaded to GitHub. Final form will be uploaded to a server to run nightly.
 
 # Dependices 
 - Beautifulsoup
--- price_scrapper.py
+++ price_scrapper.py
@@ -1,19 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.nike.com/t/pro-mens-long-sleeve-top-sDW22X/BV5594-011'
+long_sleeves = {'Grey': 'https://www.nike.com/t/pro-mens-long-sleeve-top-sDW22X/BV5594-011', 'Ocean Fog': 'https://www.nike.com/t/pro-mens-long-sleeve-top-sDW22X/BV5594-451'}
 
-page = requests.get(URL)
-print(page.status_code)
+html_class = {'reg_price':'product-price css-11s12ax is--current-price', 'sale_price':'product-price is--current-price css-s56yt7'}
 
-soup = BeautifulSoup(page.content, 'html.parser')
+def get_price(url_list, og_price):
+    ori_price = int(og_price)
+    # Pass in the URL value
+    for color, url in url_list.items():
+        page = requests.get(url)
+        #print(page.status_code)
 
-target_class = 'product-price css-11s12ax is--current-price'
+        soup = BeautifulSoup(page.content, 'html.parser')
+        
+        for x in html_class.values():
+            tags = soup.find_all(class_=x)
 
-results = soup.find(id='PDP')
+            for i in tags:
+                c_price = float(i.string[1:])
+                if c_price < ori_price:
+                    print(f'There is a sale on {color}, the price is now {i.string}')
 
-for result in results:
-    price = result.find('div', class_=target_class)
 
-    if price.text == '$35':
-        print(price.text)
+
+get_price(long_sleeves, 35)
